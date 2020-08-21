538e1f355305ec283d65d0bf865153c5994d6b02
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-08-21 08:42:28 -0400

    --no option to remove !

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
 	if (argc < 2) {
 		printf("Hello world!\n");
 	} else {
-		printf("Hello %s!\n", argv[1]);
+		if (strcmp(argv[1], "--no") == 0) {
+			printf("Hello %s\n", argv[2]);
+		} else {
+			printf("Hello %s!\n", argv[1]);
+		}
 	}
 }
